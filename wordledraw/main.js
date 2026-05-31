71e3c85356c5aa7c798b3f37ebc216022d2d5043
ShakeAndWiggle20 <th4thomas@gmail.com>
2026-05-31 15:48:41 +0200

    Added the HTML elements needed for the website and created logic for changing the Wordle answer.

--- wordledraw/main.js
+++ wordledraw/main.js
@@ -1,6 +1,7 @@
 // main.js for Wordledraw
 
 const VALID_WORDS = [];
+let answer = "";
 
 // Load valid words from the text file into the array
 fetch('./valid-wordle-words.txt')
@@ -12,3 +13,15 @@ fetch('./valid-wordle-words.txt')
     .catch(error => {
         console.error('Error loading the words:', error);
     });
+
+document.getElementById('set-button').addEventListener('click', () => {
+    // Check if input is valid
+    let input = document.getElementById('wordle-answer-input').value;
+    if (VALID_WORDS.includes(input)) {
+        document.getElementById("wordle-answer").innerHTML = `${input}`;
+        answer = input;
+    }
+    else {
+        alert("The word must be a valid Wordle answer of 5 letters!")
+    }
+});
